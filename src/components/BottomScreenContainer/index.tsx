37759c42bc0ed683dd4bcdb91225e81319e45934
NuttyJackJ <jado.jbara@gmail.com>
2022-12-23 18:30:44 +0200

    💄 Fixes in bottom container

--- src/components/BottomScreenContainer/index.tsx
+++ src/components/BottomScreenContainer/index.tsx
@@ -1,7 +1,5 @@
 import React, { PropsWithChildren } from 'react'
-import { Image, KeyboardAvoidingView, Text, View } from 'react-native'
-
-import Images from 'assets/Images'
+import { View, ViewStyle } from 'react-native'
 
 import styles from './styles'
 import MainButton from 'components/Buttons/MainButton'
@@ -9,28 +7,32 @@ import Animated, { FadeInDown, FadeOutDown, FadeOutUp } from 'react-native-reani
 
 type Props = PropsWithChildren<{
   onButtonPress?: () => void
-  buttonLabel: string
+  buttonLabel?: string
+  withBottomButton?: boolean
+  containerStyle?: ViewStyle | ViewStyle[]
 }>
 
 const BottomContainer: React.FC<Props> = ({
   children,
   buttonLabel,
-  onButtonPress
+  onButtonPress,
+  withBottomButton = true,
+  containerStyle
 }: Props) => {
   return (
     <Animated.View
       entering={FadeInDown}
       exiting={FadeOutUp}
-      style={styles.container}>
+      style={[styles.container, containerStyle]}>
       {children}
-      <View style={styles.bottomContainer}>
+      {withBottomButton && <View style={styles.bottomContainer}>
         <View style={styles.buttonContainer}>
           <MainButton
             label={buttonLabel}
             onPress={onButtonPress}
           />
         </View>
-      </View>
+      </View>}
     </Animated.View>
   )
 }
